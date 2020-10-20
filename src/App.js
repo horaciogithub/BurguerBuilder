88f6194ccb9230b2c578b77a7c82aa1bf69a3b5a
Horacio Ramirez <horacioram94@gmail.com>
2020-10-20 19:37:51 +0100

    Cleaned proyect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import Classes from'./App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2 className={Classes.Text}>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      
       </div>
     );
   }
